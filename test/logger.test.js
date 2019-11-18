9a1813cfa34a1820c4baa738e25dc140cd7cba7a
David Hoppe <hopasaurus@gmail.com>
2019-11-18 07:20:31 -0500

    clean up comments in test and added import to test to eliminate the squigly lines.

--- test/logger.test.js
+++ test/logger.test.js
@@ -1,3 +1,4 @@
+const {describe, it} = require('mocha');
 const assert = require('assert');
 
 const requireUncached = module => {
@@ -76,26 +77,15 @@ describe('createLogger with info level', function () {
 });
 
 describe('getLogger', function () {
-   it('should return the same logger created by createLogger', function () {
-      const {createLogger, getLogger} = requireUncached('../src/logger');
-      const createdLogger = createLogger();
-      const logger = getLogger();
-      assert.strictEqual(logger, createdLogger);
-   });
+    it('should return the same logger created by createLogger', function () {
+        const {createLogger, getLogger} = requireUncached('../src/logger');
+        const createdLogger = createLogger();
+        const logger = getLogger();
+        assert.strictEqual(logger, createdLogger);
+    });
 
-   it('should throw exception of createLogger was not called first', function () {
-      const {getLogger} = requireUncached('../src/logger');
-      assert.throws(() => getLogger());
-   });
+    it('should throw exception of createLogger was not called first', function () {
+        const {getLogger} = requireUncached('../src/logger');
+        assert.throws(() => getLogger());
+    });
 });
-
-// can log to debug at default log level
-
-//test('loggging to debug works when logger is created at default level', () => {
-// TODO: note that the tests will interact, logger only going to be built once
-// TODO:
-//});
-// can log to info at default log level
-// can log to warn at default log level
-// can log to error at default log level
-
